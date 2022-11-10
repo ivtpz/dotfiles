946c1ecca537febc8a37fd2585165eb606ec6ddd
Ivey Topaz <iveytopaz@gmail.com>
2022-11-10 10:25:09 -0700

    Make interactive install script

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/bash
 ############################
 # .make.sh
@@ -10,9 +8,18 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc oh-my-zsh tmux.mac.conf config/i3/config gitconfig"    # list of files/folders to symlink in homedir
-
+base_files="vimrc vim zshrc oh-my-zsh config/i3/config gitconfig"    # list of files/folders to symlink in homedir
+files=""
 ##########
+
+echo "Install on mac or linux?"
+select ml in "mac" "linux" "quit"; do
+    case $ml in
+        mac ) files="$base_files tmux.mac.config"; echo "installing for mac"; break;;
+        linux ) files="$base_files tmux.linux.config"; echo "installing for linux"; break;;
+        quit ) exit;;
+    esac
+done
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -26,8 +33,9 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
+  link=$(echo $file | sed 's/\.mac//' | sed 's/\.linux//')
   echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+  mv ~/.$link ~/dotfiles_old/
+  echo "Creating symlink from $file to .$link in home directory."
+  ln -s $dir/$file ~/.$link
 done
